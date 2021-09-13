aea063c286f19e9ea82518964b3d21509da5f95c
Felix Hu <>
2021-09-13 14:06:28 -0700

    remove blinking in videos by having two videoplayers

--- Assets/scripts/episodes/VideoEpisodeManager.cs
+++ Assets/scripts/episodes/VideoEpisodeManager.cs
@@ -7,33 +7,63 @@ public class VideoEpisodeManager : GameManager
 {
     [SerializeField] private VideoPlayer videoPlayer_;
 
+    private VideoPlayer videoPlayerA_;
+    private VideoPlayer videoPlayerB_;
+    private VideoPlayer mainVideoPlayer_;
+
     private string internalState_ = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        videoPlayer_.loopPointReached += VideoFinished;
+        videoPlayerA_ = videoPlayer_;
+
+        videoPlayerB_ = GameObject.Instantiate<VideoPlayer>(videoPlayer_);
+        videoPlayerB_.transform.SetParent(videoPlayerA_.transform.parent);
+        videoPlayerB_.transform.position = videoPlayerA_.transform.position;
+
+        videoPlayerA_.loopPointReached += VideoFinished;
+        videoPlayerB_.loopPointReached += VideoFinished;
+
+        mainVideoPlayer_ = videoPlayerA_;
     }
 
     protected override void NewNodeEventInternal(EpisodeNode node)
     {
         base.NewNodeEventInternal(node);
 
-        videoPlayer_.url = System.IO.Path.Combine(Application.streamingAssetsPath, node.VideoFilePath);
-        videoPlayer_.isLooping = false;
-        videoPlayer_.Play();
+        StartCoroutine(UpdateVideoPlayer(node.VideoFilePath, false));
 
         internalState_ = NodeState.Playing;
         UpdateNodeState(NodeState.Playing);
+    }
+
+    private IEnumerator UpdateVideoPlayer(string videoPath, bool loop)
+    {
+        VideoPlayer previousVideoPlayer = mainVideoPlayer_;
+        if (mainVideoPlayer_ == videoPlayerA_)
+        {
+            mainVideoPlayer_ = videoPlayerB_;
+        } else
+        {
+            mainVideoPlayer_ = videoPlayerA_;
+        }
+
+        mainVideoPlayer_.url = System.IO.Path.Combine(Application.streamingAssetsPath, videoPath);
+        mainVideoPlayer_.isLooping = loop;
+        mainVideoPlayer_.Play();
+
+        yield return 0;
+
+        mainVideoPlayer_.transform.SetAsLastSibling();
+        previousVideoPlayer.Stop();
     }
 
     private void VideoFinished(VideoPlayer vp)
     {
         if (string.Equals(internalState_, NodeState.Playing))
         {
-            videoPlayer_.url = System.IO.Path.Combine(Application.streamingAssetsPath, currentNode_.VideoLoopFilePath);
-            videoPlayer_.isLooping = true;
-            videoPlayer_.Play();
+            StartCoroutine(UpdateVideoPlayer(currentNode_.VideoLoopFilePath, true));
 
             internalState_ = NodeState.Looping;
             UpdateNodeState(NodeState.Looping);
