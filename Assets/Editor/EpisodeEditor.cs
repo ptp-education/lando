314d7e646180963964f84d45938f4e8996c21e0d
Felix Hu <>
2021-10-31 17:58:45 -0700

    add error message if there are duplicate nodes

--- Assets/Editor/EpisodeEditor.cs
+++ Assets/Editor/EpisodeEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 [CustomEditor(typeof(Episode))]
 public class EpisodeEditor : Editor
@@ -19,6 +20,19 @@ public class EpisodeEditor : Editor
         if (GUILayout.Button("Remove Visualize"))
         {
             myTarget.RemoveVisualize();
+        }
+
+        List<string> allNodes = new List<string>();
+        foreach(EpisodeNode n in myTarget.AllNodes)
+        {
+            allNodes.Add(n.name);
+        }
+        if (allNodes.Distinct().Count() != allNodes.Count)
+        {
+            GUIStyle s = new GUIStyle(EditorStyles.textField);
+            s.normal.textColor = Color.red;
+
+            EditorGUILayout.LabelField("Error! You cannot have duplicate node names in an episode.", s);
         }
     }
 }
