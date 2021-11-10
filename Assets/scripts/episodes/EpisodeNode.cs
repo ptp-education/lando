16a39a946041d3b981b640a936e3174085572bf7
Felix Hu <>
2021-11-10 15:45:37 -0800

    remove sequencedata code that we will most likely never use again

--- Assets/scripts/episodes/EpisodeNode.cs
+++ Assets/scripts/episodes/EpisodeNode.cs
@@ -5,93 +5,6 @@ using UnityEngine.Video;
 using System;
 using Newtonsoft.Json;
 
-public class SequenceData
-{
-    public class Object
-    {
-        public enum Type
-        {
-            Image,
-            Video,
-            Prefab,
-            Spine
-        }
-        public string Name;
-        public string ObjectType;
-        public string ModelPath;
-        public bool FlipX = false;
-        public Vector3 StartingPosition = Vector3.zero;
-        public Vector3 StartingScale = Vector3.one;
-    }
-
-    public class Movement
-    {
-        public enum Type
-        {
-            Move,
-            Scale
-        }
-
-        public string MovementType;
-        public Vector3 Target;
-        public float Duration;
-        public float RelativeTimeAfter = 0f;
-    }
-
-    public class SortingOrder
-    {
-        public string SortAbove;
-        public string SortBelow;
-    }
-
-    public class Animation
-    {
-        public string AnimationName;
-        public bool LoopForever = false;
-        public int LoopTimes = 1;
-        public float DelayTime = 0.05f;
-    }
-
-    public class Accompaniment
-    {
-        public string ObjectName;
-        public float RelativeTimeAfter;
-
-        //potential actions
-        public string SoundPath;
-        public SortingOrder SortingOrder;
-        public List<Animation> Animations = new List<Animation>();
-        public List<Movement> Movements = new List<Movement>();
-    }
-
-    public class SequenceStep
-    {
-        public enum Type
-        {
-            Wait,
-            Voiceover
-        }
-
-        public string SequenceType;
-        public float RelativeTimeAfter;
-        public float Duration;
-        public string VoiceoverPath;
-        public float VoiceoverSpeed;
-        public List<Accompaniment> Accompaniments = new List<Accompaniment>();
-    }
-
-    public class LoopInstructions
-    {
-        public string ObjectName;
-        public string Animation;
-    }
-
-    public string TemplateId = null;
-    public List<Object> Objects = new List<Object>();
-    public List<SequenceStep> SequenceSteps = new List<SequenceStep>();
-    public List<LoopInstructions> Looping = new List<LoopInstructions>();
-}
-
 public class EpisodeNode : MonoBehaviour
 {
     public enum EpisodeType
@@ -121,16 +34,6 @@ public class EpisodeNode : MonoBehaviour
     public string ImageFilePath;
     public UnityEngine.Object ImageLoop;
     public string ImageLoopFilePath;
-
-    public string SequenceData;
-    public SequenceData ProcessedSequenceData
-    {
-        get
-        {
-            SequenceData d = JsonConvert.DeserializeObject<SequenceData>(SequenceData);
-            return d;
-        }
-    }
 
     public Episode Episode
     {
