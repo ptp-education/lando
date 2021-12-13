ef5e333a50301f7ce40ec1dac2bbea0bb821b957
felix <felixhu12@gmail.com>
2021-12-12 23:41:48 -0800

    add new spawned timer object

--- Assets/scripts/episodes/EpisodeNodeObject.cs
+++ Assets/scripts/episodes/EpisodeNodeObject.cs
@@ -11,6 +11,9 @@ public class EpisodeNodeObject : MonoBehaviour
     protected EpisodeNode episodeNode_;
     protected GameManager gameManager_;
 
+    private List<SpawnedObject> spawnedPrefabs_ = new List<SpawnedObject>();
+    private float timer_ = 0f;
+
     public virtual void Init(GameManager manager, EpisodeNode node, ReadyToStartLoop callback)
     {
         gameManager_ = manager;
@@ -30,6 +33,11 @@ public class EpisodeNodeObject : MonoBehaviour
 
     public virtual void Play()
     {
+        foreach (SpawnedObject o in spawnedPrefabs_)
+        {
+            o.Reset();
+        }
+
         transform.localScale = Vector3.one;
     }
 
@@ -40,11 +48,59 @@ public class EpisodeNodeObject : MonoBehaviour
 
     public virtual void ReceiveAction(string action)
     {
-
+        foreach(SpawnedObject o in spawnedPrefabs_)
+        {
+            o.ReceivedAction(action);
+        }
     }
 
     public virtual void OnExit() 
     {
 
+    }
+
+    private void Update()
+    {
+        timer_ += Time.deltaTime;
+        //dont forget to remove objects and reset timer
+
+        foreach(EpisodeNode.PrefabSpawnObject o in episodeNode_.PrefabSpawnObjects)
+        {
+            if (timer_ > o.TimeStamp)
+            {
+                if (!o.Spawned)
+                {
+                    SpawnObject(o);
+                }
+            }
+        }
+    }
+
+    private void SpawnObject(EpisodeNode.PrefabSpawnObject prefabSpawnObject)
+    {
+        RawImage ri = GetComponentInChildren<RawImage>();
+
+        SpawnedObject o = Resources.Load<SpawnedObject>(ShareManager.PREFAB_PATH + prefabSpawnObject.Path);
+        SpawnedObject spawnedObject = GameObject.Instantiate<SpawnedObject>(o, ri.transform);
+        spawnedObject.transform.localPosition = prefabSpawnObject.Position;
+
+        spawnedPrefabs_.Add(spawnedObject);
+
+        prefabSpawnObject.Spawned = true;
+    }
+
+    private void ResetSpawnedObjects()
+    {
+        timer_ = 0f;
+        foreach (EpisodeNode.PrefabSpawnObject o in episodeNode_.PrefabSpawnObjects)
+        {
+            o.Spawned = false;
+        }
+        for (int i = 0; i < spawnedPrefabs_.Count; i++)
+        {
+            SpawnedObject o = spawnedPrefabs_[i];
+            Destroy(o.gameObject);
+        }
+        spawnedPrefabs_ = new List<SpawnedObject>();
     }
 }
