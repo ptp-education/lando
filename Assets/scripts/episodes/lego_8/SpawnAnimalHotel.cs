d302f9821332b92e04108d42dc7f853369991f08
Benjamin Zarate <benja_zarate@hotmail.es>
2022-07-06 12:44:03 -0400

    lego 8 code finish
    the base for the code is ready, Vo are missing still

--- Assets/scripts/episodes/lego_8/SpawnAnimalHotel.cs
+++ Assets/scripts/episodes/lego_8/SpawnAnimalHotel.cs
@@ -20,8 +20,7 @@ namespace Lando.Class.Lego8
         [SerializeField] private List<Sprite> objectsToPlace_; //this sprites are going to appear in the screen
         [SerializeField] private List<Image> neutralAnimals_;
 
-        private List<Sprite> objectsAlreadyInHotel = new List<Sprite>(); //this list is to check what objects have been already placed in the hotel
-
+        [SerializeField] private List<Image> hotel_;
         [SerializeField] private List<Sprite> tipJars_;
 
         [SerializeField] private GameObject neutralBackground_;
@@ -35,10 +34,18 @@ namespace Lando.Class.Lego8
         [SerializeField] private Image middleObject_;
         [SerializeField] private Image rightObject_;
 
-        [SerializeField] private List<Image> hotel_;
 
         private int currentLevel_ = 1;
         private int indexObject_;
+
+        int randomLeft_ = 0;
+        int randomMiddle_ = 0;
+        int randomRight_ = 0;
+
+        private void Start()
+        {
+            ShowCurrentAnimal();
+        }
 
         public override void ReceivedAction(string action)
         {
@@ -50,15 +57,21 @@ namespace Lando.Class.Lego8
             if (args_.Contains("success"))
             {
                 RewardSequence();
+                gameManager_.SendNewActionInternal("-update-options empty");
             }
             else if (args_.Contains("left") || args_.Contains("middle") || args_.Contains("right")) 
             {
                 SelectObjectToPlace(commandType);
+                gameManager_.SendNewActionInternal("-update-options empty");
             }
         }
 
         private void ShowCurrentAnimal() 
         {
+            gameManager_.SendNewActionInternal("-update-options default");
+            animalNeutral_ = neutralAnimals_[currentLevel_ - 1];
+            animalNeutral_.gameObject.SetActive(true);
+
             animalNeutral_.sprite = animals_[currentLevel_ - 1].neutral_;
             animalSleeping_.sprite = animals_[currentLevel_ - 1].sleeping_;
         }
@@ -66,12 +79,15 @@ namespace Lando.Class.Lego8
         private void RewardSequence() 
         {
             animalNeutral_.gameObject.SetActive(false);
+            successBackground_.gameObject.SetActive(true);
             animalSleeping_.gameObject.SetActive(true);
 
             Go.to(this, 2f, new GoTweenConfig().onComplete(t => {
+                tipJar_.gameObject.SetActive(true);
                 tipJar_.sprite = tipJars_[currentLevel_ - 1];
-                successBackground_.SetActive(true);
                 Go.to(this, 2f, new GoTweenConfig().onComplete(t => {
+                    animalSleeping_.gameObject.SetActive(false);
+                    tipJar_.gameObject.SetActive(false);
                     ShowObjectsToPlace();
                 }));
             }));
@@ -79,22 +95,67 @@ namespace Lando.Class.Lego8
 
         private void ShowObjectsToPlace() 
         {
-            int randomLeft = Random.Range(0, 4);
-            int randomMiddle = Random.Range(0, 4);
-            int randomRight = Random.Range(0, 4);
+            randomLeft_ = Random.Range(0, objectsToPlace_.Count);
+            randomMiddle_ = Random.Range(0, objectsToPlace_.Count);
+
+            while (randomMiddle_ == randomLeft_) 
+            {
+                randomMiddle_ = Random.Range(0, objectsToPlace_.Count);
+            }
+
+           randomRight_ = Random.Range(0, objectsToPlace_.Count);
+
+            while (randomRight_ == randomLeft_ || randomRight_ == randomMiddle_)
+            {
+                randomRight_ = Random.Range(0, objectsToPlace_.Count);
+            }
+
+            leftObject_.gameObject.SetActive(true);
+            middleObject_.gameObject.SetActive(true);
+            rightObject_.gameObject.SetActive(true);
+
+            leftObject_.sprite = objectsToPlace_[randomLeft_];
+            middleObject_.sprite = objectsToPlace_[randomMiddle_];
+            rightObject_.sprite = objectsToPlace_[randomRight_];
+            gameManager_.SendNewActionInternal("-update-options choose");
         }
 
         private void SelectObjectToPlace(string command) 
         {
+            leftObject_.gameObject.SetActive(false);
+            middleObject_.gameObject.SetActive(false);
+            rightObject_.gameObject.SetActive(false);
             switch (command) 
             {
                 case "left":
+                    hotel_[randomLeft_].gameObject.SetActive(true);
+                    hotel_[randomLeft_].sprite = objectsToPlace_[randomLeft_];
                     break;
                 case "middle":
+                    hotel_[randomMiddle_].gameObject.SetActive(true);
+                    hotel_[randomMiddle_].sprite = objectsToPlace_[randomMiddle_];
                     break;
                 case "right":
+                    hotel_[randomRight_].gameObject.SetActive(true);
+                    hotel_[randomRight_].sprite = objectsToPlace_[randomRight_];
                     break;
             }
+
+            //need to check if this is correct
+            //ask if once  an object appear in the options, can appear again or just remove it
+            objectsToPlace_.Remove(objectsToPlace_[randomLeft_]);
+            objectsToPlace_.Remove(objectsToPlace_[randomMiddle_]);
+            objectsToPlace_.Remove(objectsToPlace_[randomRight_]);
+
+            hotel_.Remove(hotel_[randomLeft_]);
+            hotel_.Remove(hotel_[randomMiddle_]);
+            hotel_.Remove(hotel_[randomRight_]);
+
+            Go.to(this, 2f, new GoTweenConfig().onComplete(t => {
+                successBackground_.SetActive(false);
+                currentLevel_++;
+                ShowCurrentAnimal();
+            }));
         }
     }
 }
